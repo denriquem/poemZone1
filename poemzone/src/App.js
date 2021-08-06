1abc081aceb56ef95c8161d99c31aae394d7d815
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-08-06 12:36:18 +0100

    feat: header complete

--- poemzone/src/App.js
+++ poemzone/src/App.js
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+import { Fragment } from "react";
+
 function App() {
-	return <div>Let's get cracking Vamonos!</div>;
+	return (
+		<Fragment>
+			<Header />
+			Let's get cracking Vamonos!
+		</Fragment>
+	);
 }
 
 export default App;
