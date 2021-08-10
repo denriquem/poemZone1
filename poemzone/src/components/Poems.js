c92734f508f8d28f5140aeb56f3555261c175dc2
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-08-10 13:59:20 +0100

    retrieving poems from database

--- poemzone/src/components/Poems.js
+++ poemzone/src/components/Poems.js
@@ -1,60 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Poems.module.css";
 import Card from "./UI/Card";
 import PoemItem from "./PoemItem";
 
-const poems = [
-	{
-		id: "p1",
-		title: "Blizzard",
-		author: "William Carlos Williams",
-		text: `Snow: \n
-    years of anger following \n
-    hours that float idly down \n —
-    the blizzard \n
-    drifts its weight \n
-    deeper and deeper for three days \n
-    or sixty years, eh? Then \n
-    the sun! a clutter of \n
-    yellow and blue flakes — \n
-    Hairy looking trees stand out \n
-    in long alleys \n
-    over a wild solitude. \n
-    The man turns and there — \n
-    his solitary track stretched out \n
-    upon the world.`,
-	},
-	{
-		id: "p2",
-		title: "Days",
-		author: "Phillip Larkin",
-		text: `What are days for? \n
-        Days are where we live. \n   
-        They come, they wake us \n   
-        Time and time over. \n
-        They are to be happy in: \n  
-        Where can we live but days? \n
-        
-        Ah, solving that question \n
-        Brings the priest and the doctor \n   
-        In their long coats \n
-        Running over the fields.`,
-	},
-];
-
-const poemsList = poems.map((poem) => {
-	return (
-		<PoemItem
-			id={poem.id}
-			key={poem.id}
-			title={poem.title}
-			text={poem.text}
-			author={poem.author}
-		/>
-	);
-});
-
 const Poems = (props) => {
+	console.log("hello");
+	const [poems, setPoems] = useState([]);
+
+	useEffect(() => {
+		const fetchPoems = async () => {
+			const response = await fetch(
+				"https://poemzone-176fa-default-rtdb.europe-west1.firebasedatabase.app/poems.json"
+			).then();
+
+			if (!response.ok) {
+				throw new Error("Something went wrong");
+			}
+
+			const responseData = await response.json();
+
+			const loadedPoems = [];
+
+			for (const key in responseData) {
+				loadedPoems.push({
+					id: key,
+					title: responseData[key].title,
+					author: responseData[key].author,
+					text: responseData[key].text,
+				});
+			}
+			setPoems(loadedPoems);
+		};
+
+		fetchPoems().catch((error) => {
+			throw new Error(error);
+		});
+
+		console.log(poems);
+	}, []);
+
+	const poemsList = poems.map((poem) => {
+		return (
+			<PoemItem
+				id={poem.id}
+				key={poem.id}
+				title={poem.title}
+				text={poem.text}
+				author={poem.author}
+			/>
+		);
+	});
+
 	return (
 		<section className={classes.poems}>
 			<button className={classes.buttonPosition} onClick={props.onShowAddPoem}>
