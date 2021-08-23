92113af23655e5c7b18dad6cdee4f8724f4fdd6f
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-08-23 10:53:01 +0100

    changin password with protected routes

--- poemzone/src/components/Profile/ProfileForm.js
+++ poemzone/src/components/Profile/ProfileForm.js
@@ -1,11 +1,49 @@
+import { useRef, useContext } from "react";
 import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
+
+require("dotenv").config();
 
 const ProfileForm = () => {
+	const { REACT_APP_FBASE } = process.env;
+
+	const newPasswordInputRef = useRef();
+	const authCtx = useContext(AuthContext);
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const enteredPassword = newPasswordInputRef.current.value;
+
+		fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${REACT_APP_FBASE}}
+		`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredPassword,
+					returnSecureToken: false,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		).then((res) => {
+			// assumes alway succeeds
+		});
+	};
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<div className={classes.control}>
 				<label htmlFor="new-password">New Password</label>
-				<input type="password" id="new-password" />
+				<input
+					type="password"
+					id="new-password"
+					minLength="7"
+					ref={newPasswordInputRef}
+				/>
 			</div>
 			<div className={classes.action}>
 				<button>Change Password</button>
