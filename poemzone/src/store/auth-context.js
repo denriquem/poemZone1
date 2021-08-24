7b383e3e7c64452e759d34d760f461073e96f696
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-08-24 16:11:32 +0100

    persisting user authentication status

--- poemzone/src/store/auth-context.js
+++ poemzone/src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-	const [token, setToken] = useState(null);
+	const initialToken = localStorage.getItem("token");
+	const [token, setToken] = useState(initialToken);
 
 	const userIsLoggedIn = !!token;
 
 	const logInHandlder = (token) => {
 		setToken(token);
+		localStorage.setItem("token", token);
 	};
 
 	const logOutHandler = () => {
 		setToken(null);
+		localStorage.removeItem(token);
 	};
 
 	const contextValue = {
